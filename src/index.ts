bcc0b97e92985154dc6cf2693e3e4bbcd10e9cc4
hunterreynolds16 <125692332+hunterreynolds16@users.noreply.github.com>
2023-02-19 19:43:08 -0500

    FullCalendar.js and Webflow CMS
    Note: your CMS fields MUST NOT include line breaks - this will break the JSON response.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,47 @@
-import { greetUser } from '$utils/greet';
+import { Calendar } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import listPlugin from '@fullcalendar/list';
+import timeGridPlugin from '@fullcalendar/timegrid';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  const name = 'John Doe';
-  greetUser(name);
+  const calendarElement = document.querySelector<HTMLDivElement>('[data-element="calendar"]');
+  if (!calendarElement) return;
+
+  const events = getEvents();
+  console.log({ events });
+
+  const calendar = new Calendar(calendarElement, {
+    plugins: [dayGridPlugin, timeGridPlugin, listPlugin],
+    initialView: 'dayGridMonth',
+    headerToolbar: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,timeGridWeek,listWeek',
+    },
+    events: events,
+    eventClick(data) {
+      alert(`User clicked the event ${data.event.title}`);
+    },
+  });
+  calendar.render();
 });
+
+const getEvents = (): Event[] => {
+  const scripts = document.querySelectorAll<HTMLScriptElement>('[data-element="event-data"]');
+  console.log({ scripts });
+  const events = [...scripts]
+    .map((script) => {
+      if (!script.textContent) {
+        return;
+      }
+      const event: Event = JSON.parse(script.textContent);
+      event.start = new Date(event.start);
+      event.end = new Date(event.end);
+
+      return event;
+    })
+    .filter(Boolean);
+
+  return events;
+};
